517b090cf0d60518373e89839408871796347a6c
fatykh <fatykhovv@gmail.com>
2017-01-31 18:33:15 +0300

    registration Updated

--- api/register/index.php
+++ api/register/index.php
@@ -8,6 +8,29 @@
  */
 include "../../temp/db.php";
 
+$carType = $_POST['car_type'];
+$year = $_POST['year'];
+$deviceId = $_POST['device_id'];
+$devicePlatform = $_POST['device_platform'];
+$deviceName = $_POST['device_name'];
+$phoneNumber = $_POST['phone_number'];
+$carNumber = $_POST['car_number'];
 
 
+$carTypeRegExp = "[1-7]{1}";
+$yearRegExp = "[0-9]{4}";
+$phoneNumberRegExp = "[0-9]{12}";
+$carNumberRegExp = "[А-Я]{1}[0-9]{3}[А-Я]{2}[0-9]{2,3})";
+$errors = array();
 
+if(!preg_match($carTypeRegExp, $carType)) {
+ $errors[] = "Неверный тип авто";
+}
+if(!preg_match($yearRegExp, $year)) {
+
+}
+if(!preg_match($phoneNumberRegExp, $phoneNumber)) {
+
+}if(!preg_match($carNumberRegExp, $carNumber)) {
+
+}

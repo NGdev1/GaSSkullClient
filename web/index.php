5500d7733009dd49740bc7b687f28056512e93c7
apple <andreichev.m@mail.ru>
2017-04-14 19:12:49 +0300

    Site edited

--- web/index.php
+++ web/index.php
@@ -8,21 +8,78 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <script src="res/js/jquery.min.js"></script>
+
+    <script type="text/javascript">
+
+        var isHeaderNarrow = false;
+
+        function narrowHeader() {
+            if (!isHeaderNarrow) {
+                $('.header-background-big').removeClass('header-background-big').addClass('header-background');
+                isHeaderNarrow = true;
+            }
+        }
+
+        function openHeader() {
+            if (isHeaderNarrow) {
+                $('.header-background').removeClass('header-background').addClass('header-background-big');
+                isHeaderNarrow = false;
+            }
+        }
+
+        $(document).on('scroll', function (e) {
+            if ($(document).scrollTop() < 5) {
+                openHeader();
+            }
+            else {
+                narrowHeader();
+            }
+        });
+
+    </script>
 </head>
 
 <body>
 
-    <div class="top-image">
-    <div class="image-top-logo">
-        <div class="text-top-left">Высокая гора</div>
-        <div class="text-top-right">Казань</div>
-    </div>
+<div class="header-background-big animated-element">
+    <div class="image-top-logo"></div>
+    <div class="text-top-left">Высокая гора</div>
+    <div class="text-top-right">Казань</div>
+    <div class="text-phone">+7(945)2345435</div>
+</div>
 
+<div class="gradient">
+    <div class="header-margin"></div>
+
+    <h1 class="text-main center">Автосервис Казань</h1>
+    <div style="margin: 0 0 10px 30px" class="text-site">Наш адрес: Большая красная 35а, Высокая Гора.</div>
 </div>
 
 
-<a class="button-primary center" href="admin">Admin</a>
+<div id="map"></div>
+
+<div style="padding: 30px 0" class="gradient">
+    <div class="text-site center">Вы наш постоянный клиент? Узнайте о накопленных баллах, и на что их можно потратить
+    </div>
+    <a class="button-primary center" href="admin">Узнать</a>
+</div>
+
+<div class="container center">
+    <button class="button-secondary">Прайс-лист</button>
+    <button class="button-secondary">Записаться</button>
+    <button class="button-secondary">Написать</button>
+    <button class="button-secondary">Приложение</button>
+</div>
+
+<div class="footer">
+    <div class="text-site center">Низкие цены, высокое качество.</div>
+</div>
+
+<div class="image-bottom-logo center"></div>
 <!-- /container -->
 
+<script src="res/js/map.js"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBbByzdPWzdq1FU3u9vrOWcjOUpPaGJfMA&callback=initMap"
+        async defer></script>
 </body>
 </html>
